61b1adcc724cfb5e16af246627be11056e4445a0
andev <andh1006@naver.com>
2025-08-20 11:23:48 +0900

    DisposeBag

--- SeSACRxThreads/SignUp/PasswordViewController.swift
+++ SeSACRxThreads/SignUp/PasswordViewController.swift
@@ -15,10 +15,16 @@ class PasswordViewController: UIViewController {
     let passwordTextField = SignTextField(placeholderText: "비밀번호를 입력해주세요")
     let nextButton = PointButton(title: "다음")
     
-    private let disposeBag = DisposeBag()
+    private var disposeBag = DisposeBag()
     
     private let passwordPlaceholder = Observable.just("비밀번호를 입력해주세요")
     private let nextButtonTitle = Observable.just("다음")
+    
+    //VC 클래스의 메모리가 해제되는 시점에 알아서 dispose됨
+    //DisposeBag deinit에 dispose()
+    deinit {
+        print("PasswordViewController Deinit")
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -47,11 +53,24 @@ class PasswordViewController: UIViewController {
             .bind(to: nextButton.rx.title())
             .disposed(by: disposeBag)
         
+//        nextButton.rx.tap
+//            .bind(with: self) { owner, _ in
+//                owner.navigationController?.pushViewController(PhoneViewController(), animated: true)
+//            }
+//            .disposed(by: disposeBag)
+        
         nextButton.rx.tap
-            .bind(with: self) { owner, _ in
-                owner.navigationController?.pushViewController(PhoneViewController(), animated: true)
+            .subscribe(with: self) { owner, _ in
+                print("button tap next")
+            } onError: { owner, error in
+                print("button tap onError")
+            } onCompleted: { owner in
+                print("button onCompleted")
+            } onDisposed: { owner in
+                print("button onDisposed")
             }
             .disposed(by: disposeBag)
+
 
 
     }
@@ -73,11 +92,13 @@ class PasswordViewController: UIViewController {
             }
             .disposed(by: disposeBag)
         
+        var bag = disposeBag
+        
         //infinite
         //next 이벤트가 무한해서 dispose 되지 않음.
         //화면전환이 되더라도 리소스 정리가 안되면 무한대로 실행
         let test = Observable<Int>.interval(.seconds(1), scheduler: MainScheduler.instance)
-        let result = test
+        test
             .subscribe(with: self) { owner, value in
                 print("onNext", value)
             } onError: { owner, error in
@@ -87,13 +108,14 @@ class PasswordViewController: UIViewController {
             } onDisposed: { owner in
                 print("onDisposed")
             }
+            .disposed(by: disposeBag)
         
-        DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
-            result.dispose() //리소스 정리
-        }
+//        DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+//            self.disposeBag = DisposeBag()
+//        }
         
         let test2 = Observable<Int>.interval(.seconds(1), scheduler: MainScheduler.instance)
-        let result2 = test2
+        test2
             .subscribe(with: self) { owner, value in
                 print("result2: onNext", value)
             } onError: { owner, error in
@@ -103,10 +125,11 @@ class PasswordViewController: UIViewController {
             } onDisposed: { owner in
                 print("result2: onDisposed")
             }
+            .disposed(by: disposeBag)
         
-        DispatchQueue.main.asyncAfter(deadline: .now() + 10) {
-            result.dispose()
-        }
+//        DispatchQueue.main.asyncAfter(deadline: .now() + 10) {
+//            self.disposeBag = DisposeBag()
+//        }
 
     }
     
